69e4dd60656fad41467bfd0cdb8405216ed1ec32
tahametwally <72364818+tahametwally@users.noreply.github.com>
2023-12-04 20:20:56 -0600

    Initial Commit

---
+++ CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by TAHA SALLAM on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CICD
+//
+//  Created by TAHA SALLAM on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
